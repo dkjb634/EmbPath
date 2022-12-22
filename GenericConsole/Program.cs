7f8409937e7f329161b26aebd44cb869c83ba2fe
dkjb634 <dmitry.kazantsev@jetbrains.com>
2022-12-22 10:46:53 +0200

    InitialCommit

---
+++ GenericConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+
+namespace GenericConsole
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var allResources = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            foreach (var s in allResources)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
